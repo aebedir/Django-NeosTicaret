bbf6c6e602952b84b3adb32c471cc16f429933e2
aebedir <ae.bedirr@gmail.com>
2023-05-12 22:22:56 +0300

    Kategori eklendi

--- urunler/models.py
+++ urunler/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Kategori(models.Model):
+    isim = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.isim
+
 class Urun(models.Model):
+    kategori = models.ForeignKey(Kategori, on_delete=models.SET_NULL, null=True)
     isim = models.CharField(max_length=100)
     aciklama = models.TextField(max_length=500)
     resim = models.FileField(upload_to = 'urunler/', null = True)
@@ -12,7 +19,7 @@ class Urun(models.Model):
         return self.isim
     
 
-#database relationship
+
 
 
 
--- urunler/views.py
+++ urunler/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render,redirect
 from .models import *
 from django.contrib import messages
-
+from django.db.models import Q
 # Create your views here.
 def index(request):
     urunler = Urun.objects.all()
     search=''
     if request.GET.get('search'):
         search = request.GET.get('search')
-        urunler = Urun.objects.filter(isim__icontains = search)
+        urunler = Urun.objects.filter(
+        Q(isim__icontains = search)|
+        Q(kategori__isim__icontains = search)
+        )
     context ={
         'urunler':urunler,
         'search':search
@@ -23,13 +26,16 @@ def detail(request,urunId):
     return render(request,'detail.html',context)
 
 def olustur(request):
+    kategoriler = Kategori.objects.all()
     if request.method == 'POST':
         resim = request.FILES['resim']
         isim = request.POST['isim']
         aciklama = request.POST['aciklama']
         fiyat = request.POST['fiyat']
+        kategori = request.POST['kategori']
 
         urun = Urun.objects.create(
+            kategori_id=kategori,
             isim=isim,
             aciklama=aciklama,
             fiyat=fiyat,
@@ -38,5 +44,8 @@ def olustur(request):
         urun.save()
         messages.success(request, 'Ürün Başarıyla Eklendi')
         return redirect('olustur')
+    context = {
+        'kategoriler':kategoriler
+    }
 
-    return render(request,'olustur.html')
+    return render(request,'olustur.html',context)
